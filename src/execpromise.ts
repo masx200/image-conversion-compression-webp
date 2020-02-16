546a7e7d8c4a43dab7bda11523a81efac046ef2c
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-16 16:13:06 +0800

    Update execpromise.ts

--- src/execpromise.ts
+++ src/execpromise.ts
@@ -1,6 +1,12 @@
 import child_process from "child_process";
 
 const { execFile } = child_process;
+export interface EXECERROR{
+error:child_process.ExecException
+stdout:string,
+stderr:string
+
+}
 
 export default function(cmd: string, args: string[]): Promise<{stdout:string,stderr:string}> {
     return new Promise((res, rej) => {
