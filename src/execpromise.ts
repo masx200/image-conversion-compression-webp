87f168ae8fab966906e4fc01e2fb9dcdd3eec14f
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-16 16:11:06 +0800

    Update execpromise.ts

--- src/execpromise.ts
+++ src/execpromise.ts
@@ -2,18 +2,18 @@ import child_process from "child_process";
 
 const { execFile } = child_process;
 
-export default function(cmd: string, args: string[]): Promise<string[]> {
+export default function(cmd: string, args: string[]): Promise<{stdout:string,stderr:string}> {
     return new Promise((res, rej) => {
         execFile(cmd, args, function(
-            err: child_process.ExecException | null,
+            error: child_process.ExecException | null,
             stdout,
             stderr
         ) {
-            if (err) {
-                return rej(err);
+            if (error) {
+                return rej({error,stdout, stderr});
             } else {
                 /*有的程序会往标准错误输出里写入*/
-                return res([stdout, stderr]);
+                return res({stdout, stderr});
             }
         });
     });
