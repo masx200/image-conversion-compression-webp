9e5b3aa5bee6e9720b2c68285886f6814e2a1e53
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-16 13:39:39 +0800

    Update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -6,24 +6,24 @@ import { start } from "./index.js";
 import { parseargs } from "./parse-args.js";
 
 const argsobj = parseargs(process.argv.slice(2));
-const { inputdir, outputdir } = argsobj;
-const outputmaxpixels = Number(argsobj["outputmaxpixels"]);
-if (inputdir && outputdir) {
-    const options = Object.assign({}, config, { inputdir, outputdir });
-    if (outputmaxpixels) {
-        options.outputmaxpixels = outputmaxpixels;
+const { input, output } = argsobj;
+const maxpixels = Number(argsobj["maxpixels"]);
+if (input && output) {
+    const options = Object.assign({}, config, { input, output });
+    if (maxpixels) {
+        options.maxpixels = maxpixels;
     }
     console.log(JSON.stringify(options));
     start(options);
 } else {
-    console.log("图片转换和压缩到webp ,可设置输出图片最大像素数");
-    console.log("示例:");
-    console.log(
-        `node ./dist/cli.js --inputdir=D:/baidupandownload/图片输入/  --outputdir=D:/baidupandownload/图片输出/`
+    console.error("图片转换和压缩到webp ,可设置输出图片最大像素数");
+    console.error("示例:");
+    console.error(
+        `node ./dist/cli.js --input=D:/baidupandownload/图片输入/  --output=D:/baidupandownload/图片输出/`
     );
-    console.log(
-        `node ./dist/cli.js --inputdir=D:/baidupandownload/图片输入/  --outputdir=D:/baidupandownload/图片输出/ --outputmaxpixels=4000000`
+    console.error(
+        `node ./dist/cli.js --input=D:/baidupandownload/图片输入/  --output=D:/baidupandownload/图片输出/ --maxpixels=4000000`
     );
-    console.log("输入的参数有误!");
+    console.error("输入的参数有误!");
     process.exit(1);
 }
