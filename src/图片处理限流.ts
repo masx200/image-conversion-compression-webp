ddbff465eb9bc63c8fcd71644bd2fdabe208a3d6
masx200 <34191203+masx200@users.noreply.github.com>
2020-02-17 00:28:49 +0800

    Update 图片处理限流.ts

--- src/图片处理限流.ts
+++ src/图片处理限流.ts
@@ -1,13 +1,12 @@
-const argsobj = parseargs(process.argv.slice(2));
+
+import argsobj from "./parsed-cli-options.js"
 import 创建异步限流队列, {
     statusdata
 } from "@masx200/async-task-current-limiter";
-import { parseargs } from "./parse-args.js";
-// import { argsobj } from "./cli.js";
-// import { 图片处理限流 } from "./cli.js";
+
 const 同时读取的最大文件数 = Number(argsobj["concurrent"]) || 8;
 const 图片处理限流 = 创建异步限流队列(同时读取的最大文件数);
-// import { 同时读取的最大文件数 } from "./cli.js";
+
 
 const asynclimiter = 图片处理限流;
 export default asynclimiter;
