8404b285ef8f85def5a1ec0da7e7c419e95378b5
masx200 <34191203+masx200@users.noreply.github.com>
2020-03-10 00:50:45 +0800

    Update 文件读取队列.ts

--- src/文件读取队列.ts
+++ src/文件读取队列.ts
@@ -6,9 +6,11 @@ const 同时读取的最大文件数 = 20;
 
 const 文件读取队列 = 创建异步限流队列(同时读取的最大文件数);
 
-export default 文件读取队列;
 
-//const asynclimiter = 文件读取队列;
+
+const asynclimiter = 文件读取队列;
+
+export default asynclimiter
 
 /*const listener = (data: statusdata) =>
     console.log("文件读取限流" + JSON.stringify(data));
