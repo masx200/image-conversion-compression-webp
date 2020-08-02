ef0d2a084df9f0c4c5c2cd62825abc22f192c620
masx200 <34191203+masx200@users.noreply.github.com>
2020-08-02 09:54:22 +0800

    Update execpromise.js

--- dist/execpromise.js
+++ dist/execpromise.js
@@ -1,6 +1,6 @@
 import child_process from "child_process";
 const { execFile } = child_process;
-export default function (cmd, args) {
+export default function execpromise(cmd, args) {
     return new Promise((res, rej) => {
         execFile(cmd, args, function (error, stdout, stderr) {
             if (error) {
