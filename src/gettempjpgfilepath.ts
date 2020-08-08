61387a5e7dd7ac8c9729fc8499de3f880d1aad67
masx200 <masx200@qq.com>
2020-08-09 01:13:56 +0800

    整理

--- src/gettempjpgfilepath.ts
+++ src/gettempjpgfilepath.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import {v4 as uuidv4 }from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import { tempdir } from "./resize-write.js";
 export function gettempjpgfilepath() {
     return path.resolve(tempdir, "temp-" + uuidv4() + ".jpg");
